6ae8f69a2bdefeb5b9946b395716ebe5c377fd37
Zou Jian <chsword@126.com>
2025-07-03 13:40:46 +0800

    feat: 更新项目依赖项版本，添加 System.CommandLine 包引用，优化测试项目配置

--- dotnet/dotnet/src/OpenManus.Console/obj/Debug/net9.0/OpenManus.Console.AssemblyInfo.cs
+++ dotnet/dotnet/src/OpenManus.Console/obj/Debug/net9.0/OpenManus.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpenManus.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98a4f16f2fad493e2faa52f24a020ff538128dd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9d07300c62fe58287cb3a6a82f43cc9a9a25da0")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenManus.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenManus.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
